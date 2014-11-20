6be14773820ea7f773fc020f47207bc01a7c0ac9
Isaac Cook <isaac@simpload.com>
2014-11-20 16:08:42 -0600

    Make it work with inheritence

--- cckit/_networks.py
+++ cckit/_networks.py
@@ -5,13 +5,33 @@ from . import networks
 class NetworkMetaclass(type):
     def __init__(cls, name, bases, dct):
         cls_name = cls.__name__
+
+        # Don't actually use bases, just grab their attributes for a one time
+        # copy. Kinda weird, but works for intended functionality.
+        new_dct = {}
+        for base in bases:
+            new_dct.update(base.__dict__)
+        new_dct.update(dct)
+        bases = (object,)
+
+        # Make the network class importable from our central import location
         setattr(networks, cls_name, cls)
-        for attr_name, attr in dct.items():
+        for attr_name, attr in new_dct.items():
+            # If we're inheriting from something that already wrapped their
+            # classes
+            if hasattr(attr, "_wrapped_type"):
+                attr = attr._wrapped_type
+
+            # If it's a Network specific object type, create a wrapper class
+            # that carries around the correct network reference
             if hasattr(attr, "network"):
                 new_type = type(cls_name + attr.__name__, (attr, ), {})
                 new_type.network = cls
+                # Set a reference to the wrapped class for inheritence
+                new_type._wrapped_type = attr
                 setattr(cls, attr_name, new_type)
-        super(NetworkMetaclass, cls).__init__(name, bases, dct)
+
+        super(NetworkMetaclass, cls).__init__(name, bases, new_dct)
 
 
 class Network(object):
--- cckit/bitcoin/generic.py
+++ cckit/bitcoin/generic.py
@@ -1,2 +1,5 @@
 class Transaction(object):
     network = None
+
+    def test(self):
+        print("this is a test")
