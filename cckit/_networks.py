5fcdb592582ecc2244148befb01f6d85815623db
Eric Cook <eric@simpload.com>
2015-01-28 18:32:59 -0600

    Change Network class to use Python 3 metaclass syntax

--- cckit/_networks.py
+++ cckit/_networks.py
@@ -34,8 +34,8 @@ class NetworkMetaclass(type):
         super(NetworkMetaclass, cls).__init__(name, bases, new_dct)
 
 
-class Network(object):
-    __metaclass__ = NetworkMetaclass
+class Network(object, metaclass=NetworkMetaclass):
+    pass
 
 
 class Bitcoin(Network):
